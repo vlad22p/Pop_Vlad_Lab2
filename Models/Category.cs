a4d9de08c9a3bbb6fb364794d037d33201809689
Vlad Pop <vlad.pop0797@gmail.com>
2024-10-26 03:16:14 +0300

    Lab3 final2

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,6 +5,7 @@ namespace Pop_Vlad_Lab2.Models
     public class Category
     {
         public int ID { get; set; }
+        [Display(Name = "Name")]
         public string CategoryName { get; set; }
         public ICollection<BookCategory>? BookCategories { get; set; }
     }
