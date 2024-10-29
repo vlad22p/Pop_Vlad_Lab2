b45cad8ba9747c4273a1a9dacc51773d40f7db01
Vlad Pop <vlad.pop0797@gmail.com>
2024-10-29 18:35:12 +0200

    lab4part1

---
+++ Migrations/20241029161041_modifiedAuthor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pop_Vlad_Lab2.Migrations
+{
+    /// <inheritdoc />
+    public partial class modifiedAuthor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Models/Author.cs
+++ Models/Author.cs
@@ -1,11 +1,20 @@
-﻿namespace Pop_Vlad_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pop_Vlad_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; }
     }
 }
